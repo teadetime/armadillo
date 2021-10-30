81b55e92041018ae36591f0912218496a905c213
Ian Eykamp <ian@eykamp.com>
2021-10-30 13:11:33 -0400

    wip

--- MATLAB/Motor Controller/motorSimulation3d.m
+++ MATLAB/Motor Controller/motorSimulation3d.m
@@ -2,10 +2,10 @@ L1 = 3;
 L2 = 4;
 c = 1;
 
-Px = linspace(1, 5, 100).';
+Px = linspace(1, 5, 200).';
 % Py = (Px .* -0.5 + 2);
 Py = 3 .* sin(2 .* Px) + 2;
-Pz = Px .* 0.5 .* sin(Px);
+Pz = Px .* 0.5 .* sin(Px) + 3;
 
 % T1 = (asin((Px .^ 2 + Py .^ 2 + L1 .^ 2 - L2 .^ 2) ./ (sqrt(Px .^ 2 + Py .^ 2) .* 2)) - atan(Py ./ Px));
 
@@ -13,9 +13,9 @@ a = L1;
 b = sqrt(Px .^ 2 + Py .^ 2 + Pz .^ 2); % Only major difference from 2D version
 c = L2;
 
-T1 = -(acos((a .^ 2 + b .^ 2 - c .^ 2) ./ (2 .* a .* b)) + atan(Py ./ Px)) + pi ./ 2; % Base vertical
+T1 = -(acos((a .^ 2 + b .^ 2 - c .^ 2) ./ (2 .* a .* b)) + atan(Py ./ sqrt(Px .^ 2 + Pz .^ 2))) + pi ./ 2; % Base vertical
 T2 = -acos((-cos(T1) .* L1 + Py) / L2); % Elbow
-T3 = asin(Pz / L1); % Base horizontal
+T3 = acos(Pz ./ (Px .^ 2 + Pz .^ 2)); % Base horizontal
 Angle = table(T1, T2, T3);
 
 X1 = L1 .* sin(Angle.T1) .* sin(Angle.T3);
@@ -43,13 +43,30 @@ hold on;
 plot3(0, 0, 0, 'kx', 'MarkerSize', 20);
 % plot(Position.X2, Position.Y2, 'k.');
 % plot(Px, Py, 'g-');
+
+N = [1, 0, -1];
+R = diag([1; 1; 1]) - 2 .* N.' * N; % https://math.stackexchange.com/questions/693414/reflection-across-the-plane
+RP = (R * [Px, Py, Pz].').';
+
+% plot3(RP(:, 1), RP(:, 2), RP(:, 3), 'b--');
+
+
 plot3(Px, Py, Pz, 'g-');
 plot3(armX, armY, armZ, 'r-');
+
+plot3(0, 0, 0, 'ko')
+plot3(armX(:, [1, 2]).', armY(:, [1, 2]).', armZ(:, [1, 2]).', 'r-')
+plot3(armX(:, 2).', armY(:, 2).', armZ(:, 2).', 'ko')
+plot3(armX(:, [2, 3]).', armY(:, [2, 3]).', armZ(:, [2, 3]).', 'r-')
+plot3(armX(:, 3).', armY(:, 3).', armZ(:, 3).', 'ko')
+
+
 axis equal;
 xlabel("X");
 ylabel("Y");
 zlabel("Z");
 
+% figure(2);
 % while 1
 %     for ii = 1:length(Position.X1)
 %        clf;
@@ -70,6 +87,7 @@ zlabel("Z");
 %        zlim([-(L1 + L2), (L1 + L2)]);
 %        title(sprintf("Theta 1 = %.2f, Theta 2 = %.2f", T1(ii), T2(ii)))
 %        drawnow;
-%        pause(0.03);
+%        pause(0.015);
+%        [caz, cel]
 %     end
 % end
