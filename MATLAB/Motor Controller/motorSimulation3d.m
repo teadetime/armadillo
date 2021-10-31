0f2504f035b3ff5a3ad6e6614858f64460fa0c32
Ian Eykamp <ian@eykamp.com>
2021-10-30 21:24:48 -0400

    Functional 3d cartesian to angles

--- MATLAB/Motor Controller/motorSimulation3d.m
+++ MATLAB/Motor Controller/motorSimulation3d.m
@@ -15,15 +15,15 @@ c = L2;
 
 T1 = -(acos((a .^ 2 + b .^ 2 - c .^ 2) ./ (2 .* a .* b)) + atan(Pz ./ sqrt(Px .^ 2 + Py .^ 2))) + pi ./ 2; % Base vertical
 T2 = -acos((-cos(T1) .* L1 + Pz) ./ L2); % Elbow
-T3 = asin(Py ./ (Px .^ 2 + Py .^ 2)); % Base horizontal
+T3 = asin(Py ./ sqrt(Px .^ 2 + Py .^ 2)); % Base lateral
 Angle = table(T1, T2, T3);
 
-X1 = L1 .* sin(Angle.T1) .* sin(Angle.T3);
-Y1 = L1 .* sin(Angle.T1) .* cos(Angle.T3);
+X1 = L1 .* sin(Angle.T1) .* cos(Angle.T3);
+Y1 = L1 .* sin(Angle.T1) .* sin(Angle.T3);
 Z1 = L1 .* cos(Angle.T1);
 
-X2 = X1 + L2 .* sin(-Angle.T2) .* sin(Angle.T3); % may later be T4
-Y2 = Y1 + L2 .* sin(-Angle.T2) .* cos(Angle.T3); % may later be T4
+X2 = X1 + L2 .* sin(-Angle.T2) .* cos(Angle.T3); % may later be T4
+Y2 = Y1 + L2 .* sin(-Angle.T2) .* sin(Angle.T3); % may later be T4
 Z2 = Z1 + L2 .* cos(-Angle.T2);
 
 Position = table(X1, Y1, Z1, X2, Y2, Z2);
@@ -51,43 +51,42 @@ RP = (R * [Px, Py, Pz].').';
 % plot3(RP(:, 1), RP(:, 2), RP(:, 3), 'b--');
 
 
-plot3(Px, Py, Pz, 'g-');
+plot3(Px, Py, Pz, 'go');
 plot3(armX, armY, armZ, 'r-');
 
-plot3(0, 0, 0, 'ko')
-plot3(armX(:, [1, 2]).', armY(:, [1, 2]).', armZ(:, [1, 2]).', 'r-')
-plot3(armX(:, 2).',      armY(:, 2).',      armZ(:, 2).',      'ko')
-plot3(armX(:, [2, 3]).', armY(:, [2, 3]).', armZ(:, [2, 3]).', 'r-')
-plot3(armX(:, 3).',      armY(:, 3).',      armZ(:, 3).',      'ko')
-
+plot3(0, 0, 0, 'ko');
+plot3(armX(:, [1, 2]).', armY(:, [1, 2]).', armZ(:, [1, 2]).', 'r-');
+plot3(armX(:, 2).',      armY(:, 2).',      armZ(:, 2).',      'kx');
+plot3(armX(:, [2, 3]).', armY(:, [2, 3]).', armZ(:, [2, 3]).', 'r-');
+plot3(armX(:, 3).',      armY(:, 3).',      armZ(:, 3).',      'kx');
 
 axis equal;
 xlabel("X");
 ylabel("Y");
 zlabel("Z");
 
-% figure(2);
-% while 1
-%     for ii = 1:length(Position.X1)
-%        clf;
-%        hold on;
-%        plot3(0, 0, 0, 'ko')
-%        plot3(armX(ii, [1, 2]), armZ(ii, [1, 2]), armY(ii, [1, 2]), 'r-')
-%        plot3(armX(ii, 2), armZ(ii, 2), armY(ii, 2), 'ko')
-%        plot3(armX(ii, [2, 3]), armZ(ii, [2, 3]), armY(ii, [2, 3]), 'r-')
-%        plot3(armX(ii, 3), armZ(ii, 3), armY(ii, 3), 'ko')
+figure(2);
+while 1
+    for ii = 1:length(Position.X1)
+       clf;
+       hold on;
+       plot3(0, 0, 0, 'ko');
+       plot3(armX(ii, [1, 2]), armY(ii, [1, 2]), armZ(ii, [1, 2]), 'r-');
+       plot3(armX(ii, 2),      armY(ii, 2),      armZ(ii, 2),      'ko');
+       plot3(armX(ii, [2, 3]), armY(ii, [2, 3]), armZ(ii, [2, 3]), 'r-');
+       plot3(armX(ii, 3),      armY(ii, 3),      armZ(ii, 3),      'ko');
 
-%        plot3(Px, Py, Pz, 'g-');
-% %        yline(c, '--');
+       plot3(Px, Py, Pz, 'g-');
+%        yline(c, '--');
 
-%        axis equal;
-%        view(3)
-%        xlim([-(L1 + L2), (L1 + L2)]);
-%        ylim([-1, L1 + L2]);
-%        zlim([-(L1 + L2), (L1 + L2)]);
-%        title(sprintf("Theta 1 = %.2f, Theta 2 = %.2f", T1(ii), T2(ii)))
-%        drawnow;
-%        pause(0.015);
-%        [caz, cel]
-%     end
-% end
+       axis equal;
+       view([ii .* 180 ./ length(Position.X1), ii .* 90 ./ length(Position.X1)]);
+       xlim([-(L1 + L2), (L1 + L2)]);
+       ylim([-1, L1 + L2]);
+       zlim([-(L1 + L2), (L1 + L2)]);
+       title(sprintf("Theta 1 = %.2f, Theta 2 = %.2f", T1(ii), T2(ii)))
+       drawnow;
+       pause(0.015);
+       [caz, cel]
+    end
+end
