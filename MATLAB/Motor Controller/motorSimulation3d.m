13e1228ec84886bbca4258f6c64c279012b0e032
Ian Eykamp <ian@eykamp.com>
2021-10-30 21:36:08 -0400

    change variable names

--- MATLAB/Motor Controller/motorSimulation3d.m
+++ MATLAB/Motor Controller/motorSimulation3d.m
@@ -3,35 +3,28 @@ L2 = 4;
 c = 1;
 
 Px = linspace(1, 5, 200).';
-% Pz = (Px .* -0.5 + 2);
 Py = Px .* 0.5 .* sin(Px) + 3;
 Pz = 3 .* sin(2 .* Px) + 2;
-
-% T1 = (asin((Px .^ 2 + Pz .^ 2 + L1 .^ 2 - L2 .^ 2) ./ (sqrt(Px .^ 2 + Pz .^ 2) .* 2)) - atan(Pz ./ Px));
 
 a = L1;
 b = sqrt(Px .^ 2 + Pz .^ 2 + Py .^ 2); % Only major difference from 2D version
 c = L2;
 
-T1 = -(acos((a .^ 2 + b .^ 2 - c .^ 2) ./ (2 .* a .* b)) + atan(Pz ./ sqrt(Px .^ 2 + Py .^ 2))) + pi ./ 2; % Base vertical
-T2 = -acos((-cos(T1) .* L1 + Pz) ./ L2); % Elbow
-T3 = asin(Py ./ sqrt(Px .^ 2 + Py .^ 2)); % Base lateral
-Angle = table(T1, T2, T3);
+phiBase = -(acos((a .^ 2 + b .^ 2 - c .^ 2) ./ (2 .* a .* b)) + atan(Pz ./ sqrt(Px .^ 2 + Py .^ 2))) + pi ./ 2; % Base vertical
+phiArm = -acos((-cos(phiBase) .* L1 + Pz) ./ L2); % Elbow
+thetaBase = asin(Py ./ sqrt(Px .^ 2 + Py .^ 2)); % Base lateral
+Angle = table(phiBase, phiArm, thetaBase);
 
-X1 = L1 .* sin(Angle.T1) .* cos(Angle.T3);
-Y1 = L1 .* sin(Angle.T1) .* sin(Angle.T3);
-Z1 = L1 .* cos(Angle.T1);
+X1 = L1 .* sin(Angle.phiBase) .* cos(Angle.thetaBase);
+Y1 = L1 .* sin(Angle.phiBase) .* sin(Angle.thetaBase);
+Z1 = L1 .* cos(Angle.phiBase);
 
-X2 = X1 + L2 .* sin(-Angle.T2) .* cos(Angle.T3); % may later be T4
-Y2 = Y1 + L2 .* sin(-Angle.T2) .* sin(Angle.T3); % may later be T4
-Z2 = Z1 + L2 .* cos(-Angle.T2);
+X2 = X1 + L2 .* sin(-Angle.phiArm) .* cos(Angle.thetaBase); % may later be T4
+Y2 = Y1 + L2 .* sin(-Angle.phiArm) .* sin(Angle.thetaBase); % may later be T4
+Z2 = Z1 + L2 .* cos(-Angle.phiArm);
 
 Position = table(X1, Y1, Z1, X2, Y2, Z2);
 
-
-% figure(2);
-% clf;
-% hold on;
 armX = [zeros(length(Position.X1), 1), Position.X1, Position.X2];
 armY = [zeros(length(Position.Y1), 1), Position.Y1, Position.Y2];
 armZ = [zeros(length(Position.Z1), 1), Position.Z1, Position.Z2];
@@ -39,17 +32,6 @@ armZ = [zeros(length(Position.Z1), 1), Position.Z1, Position.Z2];
 figure(1);
 clf;
 hold on;
-% plot(Position.X1, Position.Z1, 'b-');
-plot3(0, 0, 0, 'kx', 'MarkerSize', 20);
-% plot(Position.X2, Position.Z2, 'k.');
-% plot(Px, Pz, 'g-');
-
-N = [1, 0, -1];
-R = diag([1; 1; 1]) - 2 .* N.' * N; % https://math.stackexchange.com/questions/693414/reflection-across-the-plane
-RP = (R * [Px, Py, Pz].').';
-
-% plot3(RP(:, 1), RP(:, 2), RP(:, 3), 'b--');
-
 
 plot3(Px, Py, Pz, 'go');
 plot3(armX, armY, armZ, 'r-');
@@ -66,7 +48,7 @@ ylabel("Y");
 zlabel("Z");
 
 figure(2);
-while 1
+% while 1
     for ii = 1:length(Position.X1)
        clf;
        hold on;
@@ -77,16 +59,16 @@ while 1
        plot3(armX(ii, 3),      armY(ii, 3),      armZ(ii, 3),      'ko');
 
        plot3(Px, Py, Pz, 'g-');
-%        yline(c, '--');
 
        axis equal;
        view([ii .* 180 ./ length(Position.X1), ii .* 90 ./ length(Position.X1)]);
        xlim([-(L1 + L2), (L1 + L2)]);
        ylim([-1, L1 + L2]);
        zlim([-(L1 + L2), (L1 + L2)]);
-       title(sprintf("Theta 1 = %.2f, Theta 2 = %.2f", T1(ii), T2(ii)))
+       title(sprintf("Base (pan) = %.2f, Base (tilt) 2 = %.2f, Elbow (tilt) 2 = %.2f", ...
+           thetaBase(ii), phiBase(ii), phiArm(ii)))
        drawnow;
        pause(0.015);
        [caz, cel]
     end
-end
+% end
