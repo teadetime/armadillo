73e2648011be6b277e6876363c87d1efb6398e90
Ian Eykamp <ian@eykamp.com>
2021-11-05 20:47:54 -0400

    wip

--- MATLAB/Motor Controller/motorSimulation3d.m
+++ MATLAB/Motor Controller/motorSimulation3d.m
@@ -11,14 +11,19 @@ b = sqrt(Px .^ 2 + Pz .^ 2 + Py .^ 2); % Only major difference from 2D version
 c = L2;
 
 phiBase = -(acos((a .^ 2 + b .^ 2 - c .^ 2) ./ (2 .* a .* b)) + atan(Pz ./ sqrt(Px .^ 2 + Py .^ 2))) + pi ./ 2; % Base vertical
-phiArm = -acos((-cos(phiBase) .* L1 + Pz) ./ L2); % Elbow
+phiArm2 = -acos((-cos(phiBase) .* L1 + Pz) ./ L2); % Elbow
+phiArm =   acos((a .^ 2 + c .^ 2 - b .^ 2) ./ (2 .* a .* c)) + -phiBase - pi
+all(phiArm1 == phiArm2)
+phi = [phiArm1, phiArm2];
+dff = phiArm1 - phiArm2
+
 thetaBase = atan2(Py, Px); % Base lateral
 Angle = table(phiBase, phiArm, thetaBase);
 
 X1 = L1 .* sin(Angle.phiBase) .* cos(Angle.thetaBase);
 Y1 = L1 .* sin(Angle.phiBase) .* sin(Angle.thetaBase);
 Z1 = L1 .* cos(Angle.phiBase);
-
+% 
 X2 = X1 + L2 .* sin(-Angle.phiArm) .* cos(Angle.thetaBase); % may later be T4
 Y2 = Y1 + L2 .* sin(-Angle.phiArm) .* sin(Angle.thetaBase); % may later be T4
 Z2 = Z1 + L2 .* cos(-Angle.phiArm);
@@ -47,27 +52,27 @@ xlabel("X");
 ylabel("Y");
 zlabel("Z");
 
-figure(2);
-% while 1
-    for ii = 1:length(Position.X1)
-       clf;
-       hold on;
-       plot3(0, 0, 0, 'ko');
-       plot3(armX(ii, [1, 2]), armY(ii, [1, 2]), armZ(ii, [1, 2]), 'r-');
-       plot3(armX(ii, 2),      armY(ii, 2),      armZ(ii, 2),      'ko');
-       plot3(armX(ii, [2, 3]), armY(ii, [2, 3]), armZ(ii, [2, 3]), 'r-');
-       plot3(armX(ii, 3),      armY(ii, 3),      armZ(ii, 3),      'ko');
-
-       plot3(Px, Py, Pz, 'g-');
-
-       axis equal;
-       view([ii .* 180 ./ length(Position.X1), ii .* 90 ./ length(Position.X1)]);
-       xlim([-(L1 + L2), (L1 + L2)]);
-       ylim([-1, L1 + L2]);
-       zlim([-(L1 + L2), (L1 + L2)]);
-       title(sprintf("Base (pan) = %.2f, Base (tilt) 2 = %.2f, Elbow (tilt) 2 = %.2f", ...
-           thetaBase(ii), phiBase(ii), phiArm(ii)))
-       drawnow;
-       pause(0.015);
-    end
-% end
+% figure(2);
+% % while 1
+%     for ii = 1:length(Position.X1)
+%        clf;
+%        hold on;
+%        plot3(0, 0, 0, 'ko');
+%        plot3(armX(ii, [1, 2]), armY(ii, [1, 2]), armZ(ii, [1, 2]), 'r-');
+%        plot3(armX(ii, 2),      armY(ii, 2),      armZ(ii, 2),      'ko');
+%        plot3(armX(ii, [2, 3]), armY(ii, [2, 3]), armZ(ii, [2, 3]), 'r-');
+%        plot3(armX(ii, 3),      armY(ii, 3),      armZ(ii, 3),      'ko');
+% 
+%        plot3(Px, Py, Pz, 'g-');
+% 
+%        axis equal;
+%        view([ii .* 180 ./ length(Position.X1), ii .* 90 ./ length(Position.X1)]);
+%        xlim([-(L1 + L2), (L1 + L2)]);
+%        ylim([-1, L1 + L2]);
+%        zlim([-(L1 + L2), (L1 + L2)]);
+%        title(sprintf("Base (pan) = %.2f, Base (tilt) 2 = %.2f, Elbow (tilt) 2 = %.2f", ...
+%            thetaBase(ii), phiBase(ii), phiArm(ii)))
+%        drawnow;
+%        pause(0.015);
+%     end
+% % end
