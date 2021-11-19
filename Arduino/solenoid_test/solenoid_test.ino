866500a78c6484186544b91ecedae28a653fa475
Ian Eykamp <ian@eykamp.com>
2021-11-18 20:33:57 -0500

    Change to permanent on (for use with button)

--- Arduino/solenoid_test/solenoid_test.ino
+++ Arduino/solenoid_test/solenoid_test.ino
@@ -1,26 +1,28 @@
 int solenoidPin = 2;                    //This is the output pin on the Arduino
 int vacPin = 3;
 
-void setup() 
+void setup()
 {
+  Serial.begin(9600);
   pinMode(solenoidPin, OUTPUT);
   pinMode(vacPin, OUTPUT);          //Sets that pin as an output
   digitalWrite(vacPin, HIGH);
+  digitalWrite(solenoidPin, HIGH);
 }
 
-void loop() 
+void loop()
 {
 //  digitalWrite(vacPin, HIGH);
 //  delay(2000);
 //  digitalWrite(solenoidPin, LOW);      //Switch Solenoid ON
-//  delay(2000);    
+//  delay(2000);
 //  digitalWrite(vacPin,LOW);
 //  delay(2000);
 //  digitalWrite(solenoidPin, HIGH);       //Switch Solenoid OFF
 //  delay(2000);                          //Wait 5 Second
 
-  digitalWrite(solenoidPin, LOW);      //Switch Solenoid ON
-  delay(2000);    
-  digitalWrite(solenoidPin, HIGH);       //Switch Solenoid OFF
-  delay(2000);                          //Wait 1 Second
+  // digitalWrite(solenoidPin, LOW);      //Switch Solenoid ON
+  // delay(6000);
+  // digitalWrite(solenoidPin, HIGH);       //Switch Solenoid OFF
+  // delay(6000);                          //Wait 1 Second
 }
