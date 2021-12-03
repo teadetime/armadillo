4faf46df2cd142cc03ef1c09cc44bdb5dec168f4
Ian Eykamp <ian@eykamp.com>
2021-12-03 01:30:19 -0500

    add joint calibration with potentiometer

--- Arduino/roboSerialTestSendReceive/roboSerialTestSendReceive.ino
+++ Arduino/roboSerialTestSendReceive/roboSerialTestSendReceive.ino
@@ -8,6 +8,41 @@ AccelStepper stepper2(1, 5, 4);
 AccelStepper stepper3(1, 9, 8);
 Servo servoEOF;
 
+///////////////////////
+//Potentiometer Setup//
+///////////////////////
+#define pot0 A0
+#define pot1 A1
+#define pot2 A2
+#define potButton A3
+
+const bool usePotentiometerAdjust = true;
+
+int pot0val = 0;
+int pot1val = 0;
+int pot2val = 0;
+bool potButtonVal = false;
+
+
+float j1PC_adjust = 0.0;
+float j2PC_adjust = 0.0;
+float j3PC_adjust = 0.0;
+
+float potSensitivity = 60.0 / 1024;
+
+void readPots() {
+  if(usePotentiometerAdjust) {
+    pot0val = analogRead(pot0);
+    pot1val = analogRead(pot1);
+    pot2val = analogRead(pot2);
+
+    j1PC_adjust = (pot0val - 1024 / 2) * potSensitivity;
+    j2PC_adjust = (pot1val - 1024 / 2) * potSensitivity;
+    j3PC_adjust = (pot2val - 1024 / 2) * potSensitivity;
+    potButtonVal = 1 - digitalRead(potButton);
+  }
+}
+
 ///////////////////////////
 //Vars for Limit Switches//
 ///////////////////////////
@@ -40,6 +75,7 @@ const char messCharTest = 'T';
 const char messCharOther = 'O';
 const char messCharSuccess = 'Y';
 const char messCharFail = 'N';
+const char messCharCalibrate = 'B';
 
 const char startMarker = '<';
 const char endMarker = '>';
@@ -157,9 +193,9 @@ void loop() {
         //          stepper2.setSpeed(speedPC);
         //          stepper1.setAcceleration(vacPC);
         //          stepper2.setAcceleration(vacPC);
-        stepper1.moveTo(j1PC);
-        stepper2.moveTo(j2PC);
-        stepper3.moveTo(j3PC);
+        stepper1.moveTo(j1PC + j1PC_adjust);
+        stepper2.moveTo(j2PC + j2PC_adjust);
+        stepper3.moveTo(j3PC + j3PC_adjust);
         servoPos = int(j4PC)+servoZero; //This may need to be minus
         servoEOF.write(servoPos);
 
@@ -213,6 +249,14 @@ void loop() {
         break;
       default:
         break;
+      case messCharCalibrate:
+        readPots();
+        stepper1.moveTo(j1PC + j1PC_adjust);
+        stepper2.moveTo(j2PC + j2PC_adjust);
+        stepper3.moveTo(j3PC + j3PC_adjust);
+        if (potButtonVal) {
+          break;
+        }
     }
 
   }
